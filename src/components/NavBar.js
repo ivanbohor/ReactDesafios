c4736c054c4968adfb7b339040d6f0b8607ebc43
ivanbohor <ivan.olivera07@hotmail.com>
2021-11-05 16:36:17 -0300

    Desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Offcanvas,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+export default function NavB() {
+  return (
+    <Navbar bg="white" expand={false}>
+      <Container fluid>
+        <Navbar.Brand href="#">Meli Tienda</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Productos</Nav.Link>
+              <Nav.Link href="#action2">Contacto</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
